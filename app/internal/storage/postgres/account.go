4d510eee23bb1e9752b09a24ef130a84f341829d
bnavac <navajaydan@gmail.com>
2024-01-16 07:57:58 -0500

    Moved account functions to account.go

---
+++ app/internal/storage/postgres/account.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"app/internal/models"
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+func (s *PostgresStorage) GetAccount(email string) (*models.Account, error) {
+	var acc models.Account
+	err := s.db.QueryRow("SELECT id, firstname, lastname, email, password, sessionid FROM account WHERE email = $1", email).Scan(&acc.ID, &acc.Firstname, &acc.Lastname, &acc.Email, &acc.Password, &acc.SessionID)
+	if err == sql.ErrNoRows{
+		return nil, err
+	}
+	return &acc, err
+}
+//Inserts an account into the database, does not return the created account.
+func (s *PostgresStorage) CreateAccount(fname, lname, email, password string) (error){
+	_, err := s.db.Exec("INSERT INTO account VALUES (DEFAULT, null, $1, $2, $3, $4)", fname, lname, email, password)
+	return err
+}
+
+func (s *PostgresStorage) GetPassword(email string) (string, error) {
+	var password string
+	err := s.db.QueryRow("SELECT password FROM account WHERE email = $1", email).Scan(password)
+	return password, err
+}
+
+func (s *PostgresStorage) SetSessionID(email string, sessionid string) (error){
+	_, err := s.db.Exec("UPDATE account SET sessionid = $1 WHERE email = $2", sessionid, email)
+	return err
+}
