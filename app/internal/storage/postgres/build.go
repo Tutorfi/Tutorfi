442d3d76e060281d333512c0caca702dcdac74ec
Jason Ng <116290832+jason490@users.noreply.github.com>
2024-01-19 17:48:26 -0500

    Added creation of tables

--- app/internal/storage/postgres/build.go
+++ app/internal/storage/postgres/build.go
@@ -2,28 +2,71 @@ package storage
 
 import "fmt"
 import "golang.org/x/crypto/bcrypt"
+
 func (s *PostgresStorage) BuildDevDB() {
-	val := `
+	val := ` 
+    DROP TABLE IF EXISTS schedule;
+    DROP TABLE IF EXISTS user_schedule;
+    DROP TABLE IF EXISTS permission;
 	DROP TABLE IF EXISTS account;
-	CREATE TABLE account (
-	id uuid DEFAULT gen_random_uuid (),
-	sessionid uuid,
-	firstname TEXT NOT NULL,
-	lastname TEXT NOT NULL,
-	email TEXT NOT NULL,
-	password VARCHAR NOT NULL,
-	PRIMARY KEY(ID)
-	);
-	`
+    DROP TABLE IF EXISTS organization;
+    
+    CREATE TABLE organization (
+      id integer PRIMARY KEY UNIQUE,
+      setting text
+    );
+    
+    CREATE TABLE account (
+      id uuid DEFAULT gen_random_uuid () PRIMARY KEY UNIQUE,
+      organization_id integer UNIQUE,
+      email varchar UNIQUE,
+      firstname varchar,
+      lastname varchar,
+      password varchar,
+      created_at timestamp
+    );
+    
+    CREATE TABLE schedule (
+      id integer PRIMARY KEY UNIQUE,
+      organization_id integer UNIQUE
+    );
+    
+    CREATE TABLE user_schedule (
+      id integer PRIMARY KEY UNIQUE,
+      account_id uuid UNIQUE,
+      data jsonb
+    );
+    
+    CREATE TABLE permission (
+      id integer PRIMARY KEY UNIQUE,
+      account_id uuid UNIQUE,
+      permissions jsonb
+    );
+    
+    ALTER TABLE account ADD FOREIGN KEY (organization_id) REFERENCES organization (id);
+	
+    ALTER TABLE user_schedule ADD FOREIGN KEY (account_id) REFERENCES account (id)
+	on delete cascade on update cascade;
+    
+	ALTER TABLE schedule ADD FOREIGN KEY (organization_id) REFERENCES organization (id)
+	on delete cascade on update cascade;
+	
+    ALTER TABLE permission ADD FOREIGN KEY (account_id) REFERENCES account (id)
+	on delete cascade on update cascade;
+	
+	ALTER TABLE account ALTER COLUMN organization_id DROP NOT NULL;
+	ALTER TABLE user_schedule ALTER COLUMN account_id DROP NOT NULL;
+	ALTER TABLE permission ALTER COLUMN account_id DROP NOT NULL;
+    `
 	_, err := s.db.Exec(val)
 	if err != nil {
 		fmt.Println("unable to create table")
 	}
-	hash, _ := bcrypt.GenerateFromPassword([]byte ("passwordthing"), 0)
+	hash, _ := bcrypt.GenerateFromPassword([]byte("passwordthing"), 0)
 	_, err = s.db.Exec("INSERT INTO account (firstname,lastname,email,password) VALUES ('bob', 'Builder', 'bob@gmail.com', $1)", hash)
 	if err != nil {
 		fmt.Println("unable to insert values")
 		fmt.Println(err)
 	}
-	
+    fmt.Println("Finished building db")
 }
--- app/internal/storage/postgres/postgres.go
+++ app/internal/storage/postgres/postgres.go
@@ -19,13 +19,14 @@ func NewPostgresStorage(db *sql.DB) *PostgresStorage {
 func (s *PostgresStorage) GetAccount(email string) (*models.Account, error) {
 	var acc models.Account
 	err := s.db.QueryRow("SELECT id, firstname, lastname, email, password, sessionid FROM account WHERE email = $1", email).Scan(&acc.ID, &acc.Firstname, &acc.Lastname, &acc.Email, &acc.Password, &acc.SessionID)
-	if err == sql.ErrNoRows{
+	if err == sql.ErrNoRows {
 		return nil, err
 	}
 	return &acc, err
 }
-//Insert a new account into the database, returning the account that was just created
-func (s *PostgresStorage) CreateAccount(fname, lname, email, password string) (error){
+
+// Insert a new account into the database, returning the account that was just created
+func (s *PostgresStorage) CreateAccount(fname, lname, email, password string) error {
 	_, err := s.db.Exec("INSERT INTO account VALUES (DEFAULT, null, $1, $2, $3, $4)", fname, lname, email, password)
 	return err
 }
@@ -36,7 +37,7 @@ func (s *PostgresStorage) GetPassword(email string) (string, error) {
 	return password, err
 }
 
-func (s *PostgresStorage) SetSessionID(email string, sessionid string) (error){
+func (s *PostgresStorage) SetSessionID(email string, sessionid string) error {
 	_, err := s.db.Exec("UPDATE account SET sessionid = $1 WHERE email = $2", sessionid, email)
 	return err
 }
