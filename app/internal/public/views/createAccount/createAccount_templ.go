7d51695c76e07e61026a58abdfb538b1206a9300
Jason Ng <116290832+jason490@users.noreply.github.com>
2024-01-26 17:18:20 -0500

    move-to-pages

--- app/internal/public/views/createAccount/createAccount_templ.go
+++ app/internal/public/views/createAccount/createAccount_templ.go
@@ -12,7 +12,7 @@ import "bytes"
 
 import "app/internal/public/templates"
 
-func Create() templ.Component {
+func Create(cssDir []string) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
--- app/internal/public/views/login/error_templ.go
+++ app/internal/public/views/login/error_templ.go
@@ -30,7 +30,7 @@ func Error(err string) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(err)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal\public\views\login\error.templ`, Line: 3, Col: 37}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/public/views/login/error.templ`, Line: 3, Col: 37}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
