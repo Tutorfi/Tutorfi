34d98453c13364b02dd80f10e450a1a55972b934
Jason Ng <116290832+jason490@users.noreply.github.com>
2024-01-30 08:49:23 -0500

    Update scheduler.go

--- app/internal/models/scheduler.go
+++ app/internal/models/scheduler.go
@@ -5,11 +5,11 @@ import "time"
 // import "time"
 
 type Schedule struct {
-	ID        int64
+	ID             int64
 	OrganizationID int64
-    name string
-    ScheduledAt time.Time
-    time_created time.Time
+	name           string
+	ScheduledAt    time.Time
+	time_created   time.Time
 }
 
 // type UserSchedule struct {
