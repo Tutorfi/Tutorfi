66ccdb7ffc721b38b120253ccfbdf294b6721e4e
Benjamin Lee <benjaminlee755@gmail.com>
2024-03-22 20:59:59 -0400

    default calendar changes depending on mobile and desktop view

--- app/internal/public/templates/base_templ.go
+++ app/internal/public/templates/base_templ.go
@@ -62,7 +62,7 @@ func Base(cssDirectories []string, jsDirectories []string, title string) templ.C
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(title)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/public/templates/base.templ`, Line: 18, Col: 17}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal\public\templates\base.templ`, Line: 18, Col: 17}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
