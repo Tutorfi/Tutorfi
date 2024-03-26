97a3852db74acdb0267c4c75aa83933575137cf9
Benjamin Lee <benjaminlee755@gmail.com>
2024-03-26 17:49:25 -0400

    added yearly button

--- app/internal/public/views/createAccount/createAccount_templ.go
+++ app/internal/public/views/createAccount/createAccount_templ.go
@@ -118,7 +118,7 @@ func CreateAccountForm(form AccountForm, invalidInput string, isInvalid bool) te
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(invalidInput)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal\public\views\createAccount\createAccount.templ`, Line: 64, Col: 50}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/public/views/createAccount/createAccount.templ`, Line: 64, Col: 50}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
