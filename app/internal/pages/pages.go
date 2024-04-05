d353817204a8111eb4bf4164228115fd8954c789
wangd14 <wangd14@rpi.edu>
2024-04-05 17:37:28 -0400

    Update pages.go

--- app/internal/pages/pages.go
+++ app/internal/pages/pages.go
@@ -2,25 +2,10 @@ package pages
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func customHTTPErrorHandler(err error, c echo.Context) {
-	code := http.StatusInternalServerError
-	if he, ok := err.(*echo.HTTPError); ok {
-		code = he.Code
-	}
-    fmt.Print(code)
-	c.Logger().Error(err)
-    c.Logger().Print("hello1");
-	fmt.Sprintf("hello")
-    // errorPage := 
-	// if err := c.File(errorPage); err != nil {
-	// 	c.Logger().Error(err)
-	// }
-}
 
 
 func AddPagesRoutes(e *echo.Echo, user *echo.Group) {
@@ -31,7 +16,6 @@ func AddPagesRoutes(e *echo.Echo, user *echo.Group) {
     e.GET("/test", test)
     e.GET("/about", aboutPage)
     e.GET("/:errors", errorHandler)
-    e.HTTPErrorHandler = customHTTPErrorHandler
 
     user.GET("/test", test)
 }
