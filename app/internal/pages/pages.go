7d67ec92e82a530ac62218c768d2c137a53530cf
wangd14 <wangd14@rpi.edu>
2024-04-05 17:36:36 -0400

    created a 404 error page so that incorrect endpoints will be sent to that page

--- app/internal/pages/pages.go
+++ app/internal/pages/pages.go
@@ -2,9 +2,26 @@ package pages
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+	}
+    fmt.Print(code)
+	c.Logger().Error(err)
+    c.Logger().Print("hello1");
+	fmt.Sprintf("hello")
+    // errorPage := 
+	// if err := c.File(errorPage); err != nil {
+	// 	c.Logger().Error(err)
+	// }
+}
+
 
 func AddPagesRoutes(e *echo.Echo, user *echo.Group) {
     fmt.Println("Adding pages routes")
@@ -13,6 +30,8 @@ func AddPagesRoutes(e *echo.Echo, user *echo.Group) {
     e.GET("/create-account", createAccount)
     e.GET("/test", test)
     e.GET("/about", aboutPage)
+    e.GET("/:errors", errorHandler)
+    e.HTTPErrorHandler = customHTTPErrorHandler
 
     user.GET("/test", test)
 }
