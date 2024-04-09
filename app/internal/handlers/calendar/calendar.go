a4497a4de9e42d92a37504fc81933fb461d02fc3
Benjamin Lee <benjaminlee755@gmail.com>
2024-04-09 16:23:00 -0400

    adding files and demo calendar for testing purposes

--- app/internal/handlers/calendar/calendar.go
+++ app/internal/handlers/calendar/calendar.go
@@ -1,5 +1,23 @@
 package calendarhandler
 
+import (
+	"app/internal/public/views/createAccount"
+	"app/internal/public/views/login"
+	"app/internal/storage"
+	"app/internal/utils"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"regexp"
+	"time"
+	"unicode/utf8"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+	"net/mail"
+)
+
 // func (handle *calendarHandler) calendarting(){
 // 	//Return a template
 // }
---
+++ app/internal/models/calendar.go
@@ -0,0 +1,5 @@
+package models
+
+type Calendar struct {
+	
+}
