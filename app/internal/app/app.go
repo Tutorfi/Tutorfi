1be8f238a0182616e4f459d926100d62601d8f1e
Jason Ng <jassno@Jasons-Desktop>
2024-04-11 17:27:39 -0400

    moved_cookie_reading_out_postgres

--- app/internal/app/app.go
+++ app/internal/app/app.go
@@ -22,7 +22,12 @@ func NewApp(listenAddr string, store storage.Storage) *App {
 
 func (a *App) AuthMiddleware (next echo.HandlerFunc) echo.HandlerFunc {
     return func(c echo.Context) error {
-        acc, err := a.store.GetAccountSessionId(c)
+        cookie, err := c.Cookie("Tutorfi_Account")
+        if err != nil {
+            return c.Redirect(302, "/login")
+        }
+        sessionId := cookie.Value
+        acc, err := a.store.GetAccountSessionId(sessionId)
         if err != nil {
             // Oops something happened
             return c.Redirect(302, "/login")
--- app/internal/storage/storage.go
+++ app/internal/storage/storage.go
@@ -2,15 +2,12 @@ package storage
 
 import (
 	"app/internal/models"
-
-	"github.com/labstack/echo/v4"
 )
 
 type Storage interface {
 	GetAccount(email string) (*models.Account, error)
     GetGroups(acc *models.Account) ([]models.Group, error)
     GetAccountSessionId(sessionId string) (*models.Account, error)
-
 	CreateAccount(fname, lname, email, password string) error
     GetPassword(email string) (string, error)
 	SetSessionID(email string, sessionid string) error
