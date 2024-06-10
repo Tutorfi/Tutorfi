cf53694e2172f8b4f4b946f3b61d96e97d618b9f
RuikangRPI <linr12@rpi.edu>
2024-06-10 11:45:25 -0400

    post review changes
    - removed compose.yml
    - added proxy in vite server
    - moved SETUP.md info to wiki

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,26 @@ export default defineConfig({
     solidPlugin()
   ],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+        target: 'http://app:8000',
+
+        // proxy debug event listeners
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
   },
   build: {
     target: 'esnext'
