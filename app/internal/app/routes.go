58125ef7f30d0b2ea5b84563a0fb0be95bd427e2
Jason Ng <jng632121@gmail.com>
2024-06-10 22:58:56 -0400

    added_routes

--- app/internal/app/routes.go
+++ app/internal/app/routes.go
@@ -1,9 +1,11 @@
 package app
 
 import (
+	accounthandler "app/internal/handlers/account"
 	"app/internal/handlers/app/account"
 	"app/internal/handlers/app/interface"
 	"app/internal/handlers/app/scheduler"
+	interfacehandler "app/internal/handlers/interface"
 
 	"github.com/labstack/echo/v4"
 )
@@ -21,4 +23,13 @@ func addRoutes(e *echo.Echo, a *App) {
 
 	e.Static("/css", "/app/internal/public/css")
 	e.Static("/js", "/app/internal/public/js")
+}
+
+func apiRoutes(e *echo.Echo, a *App) {
+    interfaceFunctions := interfacehandler.New(a.store)
+    accounthandler := accounthandler.New(a.store)
+
+    e.GET("/api/group", interfaceFunctions.GetAccountGroups)
+    e.POST("/api/account/create", accounthandler.CreateAccount)
+    e.GET("/api/account/verify", accounthandler.Verification)
 }
