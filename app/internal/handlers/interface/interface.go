5f1cf2dadf5d231ee4f847645169aae4c0c05cf6
Jason Ng <jng632121@gmail.com>
2024-06-10 22:46:02 -0400

    added_api_for_utils

---
+++ app/internal/handlers/interface/interface.go
@@ -0,0 +1,51 @@
+package interfacehandler
+
+import (
+	"app/internal/storage"
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type InterfaceHandler struct {
+	store storage.Storage
+}
+
+func New(store storage.Storage) *InterfaceHandler {
+	return &InterfaceHandler{
+		store: store,
+	}
+}
+
+func (handle *InterfaceHandler) GetAccountGroups(c echo.Context) error {
+    cookie, err := c.Cookie("Tutorfi_Account")
+    if err != nil {
+        return c.Redirect(302, "/login")
+    }
+    sessionId := cookie.Value
+    acc, err := handle.store.GetAccountSessionId(sessionId)
+	if err == sql.ErrNoRows {
+	    return c.Redirect(302,"/login")
+    }
+	if err != nil {
+		fmt.Println(err)
+        re := fillResponse("Failed", "None", "Server error, try again later", nil, "")
+	    return c.JSON(http.StatusInternalServerError, re)
+	}
+    groups, err := handle.store.GetGroups(acc)
+    if err != nil {
+        fmt.Println(err)
+        re := fillResponse("Failed", "None", "Server error, try again later", nil, "")
+	    return c.JSON(http.StatusInternalServerError, re)
+    }
+    if len(groups) == 0 {
+        // Future update the "None" to the auth level of the tags
+        re := fillResponse("Success", "None", "No groups", nil, "")
+	    return c.JSON(http.StatusOK, re)
+    }
+    re := fillResponse("Success", "None", "", groups, "name")
+	return c.JSON(http.StatusOK, re)
+    
+}
---
+++ app/internal/handlers/interface/utils.go
@@ -0,0 +1,39 @@
+package interfacehandler
+
+import "app/internal/models"
+
+type response struct {
+	Status    string   `json:"status"`
+	Auth      string   `json:"auth"`
+	Msg       string   `json:"msg"`
+	GroupName []string `json:"groupName"`
+}
+
+func fillResponse(stat string, auth string, msg string, groups []models.Group, dataType string) response {
+	if groups == nil {
+		return response{
+			Status: stat,
+			Auth:   auth,
+			Msg:    msg,
+		}
+	}
+
+	if dataType == "name" {
+		names := make([]string, len(groups))
+		for i := 0; i < len(groups); i++ {
+			names[i] = groups[i].Name
+		}
+		return response{
+			Status:    stat,
+			Auth:      auth,
+			Msg:       msg,
+			GroupName: names,
+		}
+	}
+
+	return response{
+		Status: stat,
+		Auth:   auth,
+		Msg:    msg,
+	}
+}
