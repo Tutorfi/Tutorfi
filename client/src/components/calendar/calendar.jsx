009c20c54c25ebb320967a29d147587a253d3702
Benjamin Lee <benjaminlee755@gmail.com>
2024-07-19 19:11:52 -0400

    Revert "importing plugins and css file"
    This reverts commit b8b590cf9c744fea5eec4bad8723bfe783ea98bc.

--- client/src/components/calendar/calendar.jsx
+++ client/src/components/calendar/calendar.jsx
@@ -1,62 +1,55 @@
-import { createSignal, onMount } from 'solid-js';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import '../../../../app/internal/public/css/pages/calendar/calendar.css';
+import FullCalendar from '@fullcalendar/react'
 
-function MyCalendar() {
-  const [view, setView] = createSignal('dayGridMonth');
+function CalendarPage() {
+const [view, setView] = createSignal("dayGridMonth");
 
-  onMount(() => {
+onMount(() => {
     const script = document.createElement('script');
-    script.src = '/calendar/extensions/index.global.min.js';
+    script.src = '/extensions/index.global.min.js';
     script.onload = () => {
-      toggleVisibility();
-      window.addEventListener('resize', toggleVisibility);
+    toggleVisibility();
+    window.addEventListener("resize", toggleVisibility);
     };
     document.head.appendChild(script);
-  });
+});
 
-  function showCalendar(gridCall) {
-    const calendarEl = document.getElementById('calendar');
-    if (calendarEl) {
-      const calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [dayGridPlugin, timeGridPlugin, multiMonthPlugin],
-        dateClick: function (info) {
-          showCalendar('dayGridMonth');
-        },
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'timeGridDay,timeGridWeek,dayGridMonth,multiMonthYear',
-        },
+function showCalendar(gridCall) {
+    const calendarEl = document.getElementById("calendar");
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+    dateClick: function (info) {
+        showCalendar("dayGridMonth");
+    },
+    headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "timeGridDay,timeGridWeek,dayGridMonth,multiMonthYear",
+    },
         initialView: gridCall,
         fixedWeekCount: false,
-      });
-      calendar.render();
-    } else {
-      console.error('Calendar element not found');
-    }
-  }
-
-  function toggleVisibility() {
-    if (window.innerWidth <= 768) {
-      setView('timeGridWeek');
-      showCalendar('timeGridWeek');
-    } else {
-      setView('dayGridMonth');
-      showCalendar('dayGridMonth');
-    }
-  }
-
-  return (
-    <div>
-      <h1>Calendar</h1>
-      <center>
-        <div id="calendar"></div>
-      </center>
-    </div>
-  );
+    });
+    calendar.render();
 }
 
-export default MyCalendar;
+function toggleVisibility() {
+    if (window.innerWidth <= 768) {
+        
+        setView("timeGridWeek");
+        showCalendar("timeGridWeek");
+    } 
+    else {
+        setView("dayGridMonth");
+        showCalendar("dayGridMonth");
+    }
+}
+
+return (
+    <div>
+    <h1>Calendar</h1>
+    <center>
+        <div id="calendar"></div>
+    </center>
+    </div>
+);
+}
+
+export default CalendarPage;
