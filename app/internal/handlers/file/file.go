7c2073bb25833d6d20820acfa29193644720ebe0
Jason Ng <jng632121@gmail.com>
2024-07-26 21:12:10 -0400

    added test api endpoints for the fileserver

--- app/internal/handlers/file/file.go
+++ app/internal/handlers/file/file.go
@@ -2,25 +2,58 @@ package filehandler
 
 import (
 	"app/internal/storage"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 type FileHandler struct {
-	store storage.Storage
-    fileServerHost string
+	store          storage.Storage
+	fileServerHost string
 }
 
 func New(store storage.Storage, fileServerHost string) *FileHandler {
-    return &FileHandler{
-        store: store,
-        fileServerHost: fileServerHost,
-    }
+	return &FileHandler{
+		store:          store,
+		fileServerHost: fileServerHost,
+	}
 }
 
 // filename and path in string
 
+// Make sure to only respond with the file data
 func (handle *FileHandler) GetFile(c echo.Context) error {
+	res, err := http.Get(handle.fileServerHost + "/test/path/data.txt")
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "File server is down")
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "File server is down")
+	}
+	fmt.Println(string(body))
+	return nil
+}
 
-    return nil;
+func (handle *FileHandler) SaveFile(c echo.Context) error {
+	data := []byte("some data")
+	responseBody := bytes.NewBuffer(data)
+	res, err := http.NewRequest(http.MethodGet, handle.fileServerHost+"/test/path/data.txt", responseBody)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "File server is down")
+	}
+    defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "File server is down")
+	}
+	fmt.Println(string(body))
+	return nil
 }
--- app/internal/handlers/file/utils.go
+++ app/internal/handlers/file/utils.go
@@ -3,5 +3,11 @@ package filehandler
 type fileRequest struct {
     Filename string `json:"filename"`
     GroupId string `json:"groupId"`
+    Data string `json:"data"`
 }
 
+type res struct {
+    Status string `json:"status"`
+    Msg string `json:"msg"`
+    Data string `json:"data"`
+}
