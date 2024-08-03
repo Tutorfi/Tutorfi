24b060e7f8cefa295dec853b860d6a48a4ca60c5
Jason Ng <116290832+jason490@users.noreply.github.com>
2024-08-03 01:02:28 -0400

    adding interface

--- app/internal/handlers/interface/interface.go
+++ app/internal/handlers/interface/interface.go
@@ -31,7 +31,7 @@ func (handle *InterfaceHandler) GetAccountGroups(c echo.Context) error {
     }
 	if err != nil {
 		fmt.Println(err)
-    re := fillResponse("Failed", "None", "Server error, try again later", nil, "")
+        re := fillResponse("Failed", "None", "Server error, try again later", nil, "")
 	    return c.JSON(http.StatusInternalServerError, re)
 	}
     groups, err := handle.store.GetGroups(acc)
@@ -48,4 +48,30 @@ func (handle *InterfaceHandler) GetAccountGroups(c echo.Context) error {
     re := fillResponse("Success", "None", "", groups, "name")
 	return c.JSON(http.StatusOK, re)
     
+}
+
+func (handle *InterfaceHandler) CreateGroup(c echo.Context) error {
+    cookie, err := c.Cookie("Tutorfi_Account")
+    u := new(response)
+    err = c.Bind(u)
+    if err != nil {
+        fmt.Println(err)
+        re := fillResponse("Invalid", "None", "No cookies", nil, "")
+        return c.JSON(http.StatusUnauthorized, re)
+    }
+
+    sessionId := cookie.Value
+    acc, err := handle.store.GetAccountSessionId(sessionId)
+    
+	if err == sql.ErrNoRows {
+	    return c.Redirect(302,"/login")
+    }
+	if err != nil {
+		fmt.Println(err)
+    re := fillResponse("Failed", "None", "Server error, try again later", nil, "")
+	    return c.JSON(http.StatusInternalServerError, re)
+	}
+
+    re := fillResponse("Success", "None", "created group", nil, "name")
+    return c.JSON(http.StatusOK, re);
 }
